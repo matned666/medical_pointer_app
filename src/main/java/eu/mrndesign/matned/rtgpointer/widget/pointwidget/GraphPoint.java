82a3e7e1009c5b50fa3bd152dff389da3f0a36f8
matned <mat.niedbal@yahoo.com>
2021-06-22 21:47:29 +0200

    cleanup

--- src/main/java/eu/mrndesign/matned/rtgpointer/widget/pointwidget/GraphPoint.java
+++ src/main/java/eu/mrndesign/matned/rtgpointer/widget/pointwidget/GraphPoint.java
@@ -8,41 +8,15 @@ import static eu.mrndesign.matned.rtgpointer.utils.Variables.POINT_RADIUS;
 
 public class GraphPoint implements IGraphPoint {
 
-    private double x;
-    private double y;
     private final double startX;
     private final double startY;
     private final PointColor pointColor;
 
     public GraphPoint(double startX, double startY, PointColor pointColor) {
-        this.x = startX;
-        this.y = startY;
-        this.startX = x-POINT_RADIUS;
-        this.startY = y-POINT_RADIUS;
+        this.startX = startX-POINT_RADIUS;
+        this.startY = startY-POINT_RADIUS;
         this.pointColor = pointColor;
     }
-
-    @Override
-    public double getX() {
-        return x;
-    }
-
-    @Override
-    public double getY() {
-        return y;
-    }
-
-    @Override
-    public void setPoint(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public PointColor getPointColor() {
-        return pointColor;
-    }
-
 
     @Override
     public void draw(GraphicsContext context) {
